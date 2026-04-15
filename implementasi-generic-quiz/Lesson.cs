8a220c48019d8b83c807053b253b97e03a36bce4
AdityaAdnyana <hiddenaditya4@gmail.com>
2026-04-15 12:08:46 +0700

    feat: menambahkan implementasi generic. program file ImplementasiGenericQuiz

---
+++ implementasi-generic-quiz/Lesson.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImplemantasiGenericQuiz
+{
+    internal class Lesson
+    {
+
+        public List<IQuestion> questions { get; set; } = new List<IQuestion>();
+       
+        public void StartSession()
+        {
+            //SessionManager sessionManager = SessionManager.Instance;
+
+            //sessionManager.InitializeNewSession(this);
+
+            //sessionManager.ChangeState(SessionState.PresentingQuestion);
+
+            Console.WriteLine("START SESSION");
+            foreach (var question in questions)
+            {
+                Console.WriteLine(question.QuestionText);
+                var userAnswer = Console.ReadLine();
+                bool isCorrect = question.ValidateAnswer(userAnswer);
+
+                Console.WriteLine(question.getAnswer);
+
+                if (isCorrect) Console.WriteLine("JAWABAN BENAR!");
+                else Console.WriteLine("JAWABAN SALAH");
+            }
+        }
+    }
+
+    
+}
---
+++ implementasi-generic-quiz/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ImplemantasiGenericQuiz;
+
+public class Program
+{
+    public static void Main()
+    {
+        TempLessonInit dummyLesson = new();
+    }
+}
+    
