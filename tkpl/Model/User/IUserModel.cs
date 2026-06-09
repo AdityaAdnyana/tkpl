87bbe15b75f6b76ff7a55c360b48f74abfe9e41a
Farid-lol <farid.j.o.g@gmail.com>
2026-06-09 20:33:21 +0700

    temp penambahan User,UserModel,IUserModel,ReportQuiz

---
+++ tkpl/Model/User/IUserModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tkpl.Model.User
+{
+    interface IUserModel
+    {
+
+        void SingUp(string username, string password);
+        void Login(string username, string password);
+
+        void Logout();
+
+        int GetUserId();
+
+        string GetUserName();
+        void SetUsername(string username);
+
+        string GetPassword();
+
+        void SetPassword(string password);
+    }
+}
