062c7cf27b233e43952a42e02713fe5ea2804121
AdityaAdnyana <hiddenaditya4@gmail.com>
2026-06-15 00:44:03 +0700

    delete: menghapus fungsi yang tidak digunakan, fix: memindahkan fungsi event tombol ke controller agar memenuhi SRP

--- tkpl/View/Homepage/Homepage.cs
+++ tkpl/View/Homepage/Homepage.cs
@@ -14,24 +14,9 @@ namespace tkpl.View
             _controller = new HomepageController(this);
         }
 
-        private void btnStartLevel1_Click(object sender, EventArgs e)
-        {
-            _controller.LoadQuizSession(0, 0);
-        }
-
-        private void btnStartLevel2_Click(object sender, EventArgs e)
-        {
-            _controller.LoadQuizSession(0, 1);
-        }
-
-        private void btnStartLevel3_Click(object sender, EventArgs e)
-        {
-            _controller.LoadQuizSession(0, 2);
-        }
-
-        private void btnExit_Click(object sender, EventArgs e)
-        {
-            _controller.ExitApplication();
-        }
+        public Button GetBtnStartLevel1() => btnStartLevel1;
+        public Button GetBtnStartLevel2() => btnStartLevel2;
+        public Button GetBtnStartLevel3() => btnStartLevel3;
+        public Button GetBtnExit() => btnExit;
     }
 }
